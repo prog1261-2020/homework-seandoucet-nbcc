0f4cc0939925d293fa91c98387ca4cf638d51309
seandoucet-nbcc <55193396+seandoucet-nbcc@users.noreply.github.com>
2020-03-17 14:27:24 -0300

    More things updated

---
+++ Homework03/Homework03/Homework03/Quick_Sort_Main_Program.cpp
@@ -0,0 +1,29 @@
+/*
+Copyright 2020 Sean Doucet
+All Rights Reserved
+*/
+
+// C++ Standard Library Pre Processor
+#include <iostream>
+#include <vector>
+
+int main() {
+	std::cout << "PROGRAM Executed..." << std::endl;
+
+	// Vector Storage
+	std::vector<int> list = { 2, 1, 4, 6, 3, 7 };
+
+	// Pivot Variable
+	int pivot{0};
+
+	// Quicksort Algorithm 
+	list.swap;
+
+	//for (int count = 0; count >= count; ++count) {
+		//list.push_back(1);
+	//}
+
+	std::cout << pivot;
+
+	return 0;
+}
